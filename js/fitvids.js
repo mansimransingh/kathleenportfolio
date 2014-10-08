e227c93d57925b3c52febf84200dbdd02a87a060
Kathleen Warner <kathleenhwarner@gmail.com>
2014-10-08 14:52:16 -0400

    test

--- js/fitvids.js
+++ js/fitvids.js
@@ -1,5 +1,5 @@
-<script src="path/to/jquery.min.js"></script>
-<script src="path/to/jquery.fitvids.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.fitvids.js"></script>
 <script>
   $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
