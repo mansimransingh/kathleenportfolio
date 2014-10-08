39d951b2bb203e5b0aaa44508e1231e433ac3416
Kathleen Warner <kathleenhwarner@gmail.com>
2014-10-08 14:34:37 -0400

    test

--- js/fitvids.js
+++ js/fitvids.js
@@ -1,7 +1,7 @@
 <script src="path/to/jquery.min.js"></script>
 <script src="path/to/jquery.fitvids.js"></script>
 <script>
-  $(document).ready(function(){
+  $(document).iframe (function(){
     // Target your .container, .wrapper, .post, etc.
     $("#thing-with-videos").fitVids();
   });
